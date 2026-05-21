14e5b57195f58ae81517d38d9c8c078a36c71930
Sandra <sandramathias@gmail.com>
2026-05-21 19:05:04 -0300

    Telas funcionais com API Render funcionando

--- app/src/main/java/com/g1/fidelitasapp/di/NetworkModule.kt
+++ app/src/main/java/com/g1/fidelitasapp/di/NetworkModule.kt
@@ -9,14 +9,15 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
 
-    // IP especial do emulador para acessar o localhost da máquina física
-    private const val BASE_URL = "http://10.0.2.2:3000/"
+    // IP especial do emulador para acessar a API no Render
+    private const val BASE_URL = "https://fidelitasapi.onrender.com/"
 
     @Provides
     @Singleton
@@ -26,6 +27,9 @@ object NetworkModule {
         }
         return OkHttpClient.Builder()
             .addInterceptor(logger)
+            .connectTimeout(60, TimeUnit.SECONDS)
+            .readTimeout(60, TimeUnit.SECONDS)
+            .writeTimeout(60, TimeUnit.SECONDS)
             .build()
     }
 
@@ -46,27 +50,3 @@ object NetworkModule {
     }
 }
 
-
-//import dagger.Module
-//import dagger.Provides
-//import dagger.hilt.InstallIn
-//import dagger.hilt.components.SingletonComponent
-//import retrofit2.Retrofit
-//import retrofit2.converter.gson.GsonConverterFactory
-//import javax.inject.Singleton
-
-
-
-//@Module
-//@InstallIn(SingletonComponent::class)
-//object NetworkModule {
-//
-//    @Provides
-//    @Singleton
-//    fun provideRetrofit(): Retrofit {
-//        return Retrofit.Builder()
-//            .baseUrl("https://api.example.com/") // Replace with your actual base URL
-//            .addConverterFactory(GsonConverterFactory.create())
-//            .build()
-//    }
-//}
